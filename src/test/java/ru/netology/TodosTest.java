84d7919e1db4dcc46f7024c22e737f1712fd51e0
Suhodolov Matwey <Msuxodolov18@gmail.com>
2024-02-16 20:38:21 +0300

    change src/test/java/ru/netology/TodosTest.java

--- src/test/java/ru/netology/TodosTest.java
+++ src/test/java/ru/netology/TodosTest.java
@@ -127,5 +127,59 @@ public class TodosTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+    public void TheTaskSearchMethodIsATest() {
+        Task task = new Task(6);
+        SimpleTask simpleTask = new SimpleTask(555, "купить молоко и принести домой");
+
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(555, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выпить молоко",
+                "Помыть посуду",
+                "Открыть холодильник"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(task);
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        boolean[] expected = {false, true, false, true};
+        boolean[] actual = new boolean[4];
+
+        Task[] arrayTask = todos.findAll();
+
+        for (int i = 0; i < arrayTask.length; i++) {
+            actual[i] = arrayTask[i].matches("молоко");
+        }
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testEquals() {
+        Task task1 = new Task(1);
+        Task task2 = new Task(1);
+
+        Assertions.assertTrue(task1.equals(task1));
+
+        Assertions.assertTrue(task1.equals(task2));
+
+        Assertions.assertFalse(task1.equals(null));
+
+        Assertions.assertFalse(task1.equals("task"));
+
+        Task task3 = new Task(2);
+
+        Assertions.assertFalse(task1.equals(task3));
+    }
+
 
 }
+
+
